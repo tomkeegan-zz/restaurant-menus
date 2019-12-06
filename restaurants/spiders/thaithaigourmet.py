b54dbd0940cd458ffb663d19ac47570fab8981f1
Tom Keegan <tomkeegan835@gmail.com>
2019-12-06 09:09:07 +0000

    finally crawling thaithaigourmet's beyondmenu sidte, but it gets stuck scraping the first item over and over again, the some response gets printed in the middle of the data

--- restaurants/spiders/thaithaigourmet.py
+++ restaurants/spiders/thaithaigourmet.py
@@ -4,19 +4,26 @@ from scrapy import Request
 
 class ThaithaigourmetSpider(scrapy.Spider):
     name = 'thaithaigourmet'
-    allowed_domains = ['www.thaithaigourmet.com/']
+    allowed_domains = ['thaithaigourmet.com', 'beyondmenu.com']
     start_urls = ['http://www.thaithaigourmet.com/']
 
     def parse(self, response):
         menu_url = response.css('.logo').xpath('@rel').get()
+        print('\n\n\nRedirecting to ' + menu_url + '\n\n\n')
         yield Request(menu_url, callback=self.parse_menu)
-        pass  
+        pass
 
     def parse_menu(self, response):
-        menu_items = response.css('.items_wrapper').xpath('.//h4/text()').getall()
-        for item in menu_items:
-            print(item)
-        yield {
-            'menu_items': menu_items
-        }
+        items_wrapper_array = response.css('.items_wrapper')
+        for items_wrapper in  items_wrapper_array:
+            items = items_wrapper.xpath('a')
+            for item in items:
+                name = item.xpath('//h4/text()').extract_first()
+                description = item.xpath('//p/text()').extract_first()
+                price = item.xpath('//td[@class="price"]/text()').extract_first()
+                yield {
+                    'name': name,
+                    'description': description,
+                    'price': price
+                }       
         pass
